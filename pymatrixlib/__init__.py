6bf30739aa674afd769bc96d03a5d8bcd5d1f454
Daniel Copley <djcopley@users.noreply.github.com>
2020-04-26 14:29:20 -0700

    Added setup.py and updated gitignore

---
+++ pymatrixlib/__init__.py
@@ -0,0 +1,1 @@
+from pymatrixlib.pymatrixlib import *
--- setup.py
+++ setup.py
@@ -1,5 +1,30 @@
 from setuptools import setup
 
 setup(
+    author="Daniel Copley",
+    author_email="djrcopley@gmail.com",
 
+    name="pymatrixlib",
+    license="GPLv3",
+    description="A python-based matrix library.",
+    keywords=["linear algebra", "matrix", "calculator", "library", "python3"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Topic :: Application",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+    ],
+    python_requires=">=3=4",
+    setup_requires=[
+        "setuptools_scm"
+    ],
+    install_requires=[
+        "numpy",
+    ],
+    packages=[
+        "pymatrixlib",
+    ],
+    use_scm_version={
+        "relative_to": __file__,
+        "write_to": "pymatrixlib/version.py"
+    }
 )
